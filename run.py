fb9b1651f4eee1ed4cf4417f09bdcba850ee1e0e
vivienrauch <vivienmirjam@gmail.com>
2023-02-28 13:56:55 +0000

    Initial commit, design hangman

--- run.py
+++ run.py
@@ -1,3 +1,92 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import os
+import words from words
+
+def hangman_stages(attempts):
+    """
+    Displays the stages of hangman based on the number of attempts the user has left.
+    """
+    hangman_stages = [
+
+        """
+        You ran out of attempts, game over!
+             __________
+             |       |
+             |       O
+             |      \|/
+             |       |  
+             |      / \
+         ____|____  
+        """,
+        """
+        You have 1 more guess left!
+             __________
+             |       |
+             |       O
+             |      \|/
+             |       |  
+             |      / 
+         ____|____  
+        """,
+        """
+        You have 2 more guesses left!
+             __________
+             |       |
+             |       O
+             |      \|/
+             |       |  
+             |       
+         ____|____  
+        """,
+        """
+        You have 3 more guesses left!
+             __________
+             |       |
+             |       O
+             |      \|/
+             |         
+             |       
+         ____|____  
+        """,
+        """
+        You have 4 more guesses left!
+             __________
+             |       |
+             |       O
+             |      \|
+             |         
+             |       
+         ____|____  
+        """,
+        """
+        You have 5 more guesses left!
+             __________
+             |       |
+             |       O
+             |       |
+             |         
+             |       
+         ____|____  
+        """,
+        """
+        You have 6 more guesses left!
+             __________
+             |       |
+             |       O
+             |      
+             |         
+             |       
+         ____|____  
+        """,
+        """
+        You have 7 guesses to find out the correct word!
+             __________
+             |       |
+             |       
+             |      
+             |         
+             |       
+         ____|____  
+        """
+    ]
+    return hangman_stages[attempts]
